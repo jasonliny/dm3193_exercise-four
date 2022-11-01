d985e1d4de105882693ed9da17ddcaacf4ea84b9
Jason <70929180+jasonliny@users.noreply.github.com>
2022-11-01 08:35:01 -0400

    Setup route for serving static files

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/images", express.static("public"));
 
 app.listen(port, () => console.log("Working!"));
